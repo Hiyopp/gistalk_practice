1b10b56d80a10c857fa58326f5750def50cbcaf4
Hiyopp <2005jonghyun@gm.gist.ac.kr>
2024-07-11 23:00:31 +0900

    last save

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,12 @@
-import {useState, useEffect} from "react";
-import styled from "styled-components";
-import axios from 'axios';
+import axios from "axios";
+import { useState } from "react";
 
 type dataType = {
-  "userId": number;
-  "id": number;
-  "title": string;
-  "body": string;
-}
+  userId: number;
+  id: number;
+  title: string;
+  body: string;
+};
 
 function App() {
   const [datas, setDatas] = useState<dataType[] | null>(null);
@@ -20,9 +19,7 @@ function App() {
     setDatas(null);
     // loading 상태를 true 로 바꿉니다.
     setLoading(true);
-    const response = await axios.get(
-      './data/mockData'
-    );
+    const response = await axios.get("/data/mockData");
     setDatas(response.data); // 데이터는 response.data 안에 들어있습니다.
     console.log(response.data);
   } catch (e: any) {
@@ -34,11 +31,7 @@ function App() {
   if (error) return <div>에러가 발생했습니다</div>;
   if (!datas) return null;
 
-  return (
-    <div>
-      {datas}
-    </div>
-  );
+  return <div>{datas[0].title}</div>;
 }
 
 export default App;
--- src/data/mockData.ts
+++ src/data/mockData.ts
@@ -1,15 +1,14 @@
-const data = 
- [
- {
-    "userId": 1,
-    "id": 1,
-    "title": "test title one",
-    "body": "test body one"
+export const data = [
+  {
+    userId: 1,
+    id: 1,
+    title: "test title one",
+    body: "test body one",
   },
-   {
-    "userId": 2,
-    "id": 2,
-    "title": "test title two",
-    "body": "test body two"
-  }
-  ]
+  {
+    userId: 2,
+    id: 2,
+    title: "test title two",
+    body: "test body two",
+  },
+];
